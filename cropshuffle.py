6fe5bd630ef72de1325d92291fb01008f2cd3dd2
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:42:57 +0300

    Use aac codec

--- cropshuffle.py
+++ cropshuffle.py
@@ -1,13 +1,24 @@
-import moviepy.editor
+import moviepy.editor as mpe
 import os
 import random
 import fnmatch
 from pydub import AudioSegment
 
-PATH = '/Users/andrey.matveev/input.mp4'
+PATH = '/Users/andrey.matveev/VIDEO/input.mp4'
+PATH_AUDIO = '/Users/andrey.matveev/VIDEO/input.m4a'
+PATH_VIDEO_AUDIO = '/Users/andrey.matveev/VIDEO/input.mp4'
 SEGMENTS = 10
 
-clip = moviepy.editor.VideoFileClip(PATH)
+
+def add_audio_to_video():
+    init_clip = mpe.VideoFileClip(PATH)
+    audio_background = mpe.AudioFileClip(PATH_AUDIO)
+    # final_audio = mpe.CompositeAudioClip([clip.audio, audio_background])
+    final_clip = init_clip.set_audio(audio_background)
+    return final_clip
+
+
+clip = add_audio_to_video()
 part_len = clip.duration / SEGMENTS
 
 outputs = []
@@ -18,6 +29,6 @@ for i in range(0, SEGMENTS):
     outputs.append(out_clip)
 
 random.shuffle(outputs)
-out = moviepy.editor.concatenate_videoclips(outputs)
-out.write_videofile('out.mp4')
+out = mpe.concatenate_videoclips(outputs)
+out.write_videofile('out.mp4', codec= 'mpeg4', audio_codec='aac')
 
