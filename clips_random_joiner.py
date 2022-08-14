248d39ddc1a5c6c870acef2fa2d6372c91b5b246
pomoq-dev <poqdev@gmail.com>
2022-08-14 19:42:57 +0300

    Add ads to the clips

--- clips_random_joiner.py
+++ clips_random_joiner.py
@@ -5,6 +5,8 @@ import moviepy.editor as mpe
 
 import video_tools
 
+ads_clip = mpe.VideoFileClip('ExtensionAds.mp4')
+
 clips_paths = list(filter(lambda f: f.startwith('video'), os.listdir('clips')))
 random.shuffle(clips_paths)
 clips = []
@@ -14,5 +16,7 @@ for clip_path in clips_paths:
     clips.append(clip)
 
     if len(clips) == 20:
+        where_to_ads_i = random.randint(2, 8)
+        clips.insert(where_to_ads_i, ads_clip)
         video_tools.join_clips_to_video(clips, f'result{video_i}')
         clips.clear()
