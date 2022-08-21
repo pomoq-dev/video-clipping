15c7ab15e22dc6313edb114e5241c88885ede175
pomoq-dev <poqdev@gmail.com>
2022-08-21 19:08:01 +0300

    Add join audios

--- vidlib/audio_tools.py
+++ vidlib/audio_tools.py
@@ -3,6 +3,7 @@ import random
 
 from moviepy import editor as mpe
 from pydub import AudioSegment
+from tqdm import tqdm
 
 
 def split_audio_to_parts(audio, piece_duration=3):
@@ -45,3 +46,36 @@ def m4a2wav(m4a_path, wav_path, remove_original=False):
     sound.export(wav_path, format='wav')
     if remove_original:
         os.remove(m4a_path)
+
+
+def join_wav_audios(audio_clip_paths, output_path, verbose=1):
+    """
+        Concatenates two or more audio files into one audio file using PyDub library
+        and save it to `output_path`. A lot of extensions are supported, more on PyDub's doc.
+        """
+
+    def get_file_extension(filename):
+        """A helper function to get a file's extension"""
+        return os.path.splitext(filename)[1].lstrip(".")
+
+    clips = []
+    # wrap the audio clip paths with tqdm if verbose
+    audio_clip_paths = tqdm(audio_clip_paths, "Reading audio file") if verbose else audio_clip_paths
+    for clip_path in audio_clip_paths:
+        # get extension of the audio file
+        extension = get_file_extension(clip_path)
+        # load the audio clip and append it to our list
+        clip = AudioSegment.from_file(clip_path, extension)
+        clips.append(clip)
+
+    final_clip = clips[0]
+    range_loop = tqdm(list(range(1, len(clips))), "Concatenating audio") if verbose else range(1, len(clips))
+    for i in range_loop:
+        # looping on all audio files and concatenating them together
+        # ofc order is important
+        final_clip = final_clip + clips[i]
+    # export the final clip
+    final_clip_extension = get_file_extension(output_path)
+    if verbose:
+        print(f"Exporting resulting audio file to {output_path}")
+    final_clip.export(output_path, format=final_clip_extension)
