ecdcfaa2dea6272a014a269b700a39e52eff7a44
pomoq-dev <poqdev@gmail.com>
2022-08-26 10:36:00 +0300

    Add image cropper

---
+++ examples/image_cropper.py
@@ -0,0 +1,21 @@
+import math
+import os.path
+
+from vidlib import files_tools
+from PIL import Image
+
+DIRECTORY = '../images/millitary_equipment'
+RES_DIRECTORY = '../images_new'
+files_tools.clear_directory(RES_DIRECTORY)
+
+images = files_tools.list_full_paths(DIRECTORY, only_format='.png')
+for image_path in images:
+    img = Image.open(image_path)
+    indent = 1/68 * img.width
+    box = (math.floor(indent), math.floor(indent), math.floor(img.width - indent), math.floor(img.height - indent))
+    img2 = img.crop(box)
+
+    basename = os.path.basename(image_path)
+    res_path = os.path.join(RES_DIRECTORY, basename)
+    img2.save(res_path)
+
