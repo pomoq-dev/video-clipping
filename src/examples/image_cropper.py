d5efcbbf7a0d18e62f0c07dda9c6cc71ac04a76a
pomoq-dev <poqdev@gmail.com>
2022-09-25 23:55:44 +0300

    Change image cropper

--- src/examples/image_cropper.py
+++ src/examples/image_cropper.py
@@ -1,15 +1,17 @@
 import math
 import os.path
 
+from tqdm import tqdm
+
 from src.vidliboba import files_tools
 from PIL import Image
 
-DIRECTORY = '..'
-RES_DIRECTORY = '../images_new_girl_new'
+DIRECTORY = '../../Hobana'
+RES_DIRECTORY = '../../Obana'
 files_tools.clear_directory(RES_DIRECTORY)
 
 images = files_tools.list_full_paths(DIRECTORY, only_format='.png')
-for image_path in images:
+for image_path in tqdm(images):
     img = Image.open(image_path)
     indent = 1/68 * img.width
     box = (math.floor(indent), math.floor(indent), math.floor(img.width - indent), math.floor(img.height - indent))
